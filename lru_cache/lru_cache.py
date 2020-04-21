1e1beae455d712f84ba5a530ae44c81bcfbe4676
samuel <osamaimafidon@gmail.com>
2020-04-21 18:25:24 +0100

    mvp complete

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('./doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +12,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.current_no_of_nodes = 0
+        self.cache = {}
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +25,13 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        node = self.storage.head
+        while node:
+            if key == node.value.get(key, None):
+                self.storage.move_to_end(node)
+                break
+            node = node.next
+        return self.cache.get(key, None)
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +44,32 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if self.current_no_of_nodes == self.limit:
+            self.storage.remove_from_head()
+            self.current_no_of_nodes -= 1
+            
+        if key in self.cache:
+            node = self.storage.head
+
+            while node:
+                if key == node.value.get(key, None):
+                    node.value[key] =  value
+                    break
+                node = node.next
+
+        self.storage.add_to_tail({key: value})
+        self.current_no_of_nodes += 1
+        self.cache[key] = value
+
+    def __str__(self):
+        return f'storage: {self.storage.head.value} cache: {self.cache}'
+
+cache = LRUCache(3)
+cache.set('item1', 'a')
+# cache.set('item2', 'b')
+# cache.set('item3', 'c')
+# cache.set('item4', 'd')
+# cache.set('item4', 'D')
+# cache.set('item5', 'e')
+# print(cache.__str__())
+print(cache.get('nonexistent'))
